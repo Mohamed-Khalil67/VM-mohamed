a407c285061469c271b8bd58156c8f40cdd58a68
Mohamed Khalil <71330579+Mohamed-Khalil67@users.noreply.github.com>
2020-12-08 17:19:35 +0100

    Add files via upload

---
+++ ShellSCripting/Testing scripts/cmd.sh
@@ -0,0 +1,7 @@
+echo "Put X: "
+read x
+echo "Put Y: "
+read y
+
+sum=$(($x+$y))
+echo "Sume=$sum"
---
+++ ShellSCripting/Testing scripts/nameUser.sh
@@ -0,0 +1,5 @@
+read -p "Enter ur name: " str1
+
+date1=$(date)
+
+echo "name user = $str1 , date = $date1"
